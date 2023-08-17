b3d58935ba0434d6b773880d4753f8b33d2d129f
Souvik Kundu <kivtech88@gmail.com>
2023-08-18 00:22:39 +0530

    Update else-if-fizzbuzz.c

--- else-if-fizzbuzz.c
+++ else-if-fizzbuzz.c
@@ -15,3 +15,31 @@ int main(int argc, char **argv)
   }
   return 0;
 }
+
+// alternate (better) solution
+
+/*
+#include <stdio.h>
+ 
+int main(int argc, char **argv)
+{
+  int i;
+  for (i=1; i<=100; i++)
+ {
+    //if the number divides, remainder will be 0 
+    // we use the ! to change it to 1 so that the overall condition 
+    //becomes TRUE
+      if (!(i % 3) && !(i % 5)) //checking if it divides by 3 AND 5 by taking % (modulus)
+	printf("FizzBuzz");   
+      else if (!(i % 3)) //checking if it divides by just 3 
+	printf("Fizz");
+      else if (!(i % 5)) //checking if it divides by just 5
+	printf("Buzz");
+      else
+	printf("%d", i);
+      printf("\n");
+ }
+  return 0;
+}
+
+*/
